3bcbb162d4523570c66c0577b076e2de7907d33a
Dennis Liebenberg <40003681+thunderstorm115@users.noreply.github.com>
2019-10-18 12:23:33 +0200

    Upload Stack Project
    This is a project demonstrating how stacks work

---
+++ Stacks_Example/src/stacks_example/Stacks_Example.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stacks_example;
+
+/**
+ *
+ * @author dennis liebenberg Student Number: QFJDGRL93
+ *
+ */
+public   class Stacks_Example {
+
+  
+
+   
+    public static void main(String[] args) {
+      
+      
+      
+    }
+
+}
